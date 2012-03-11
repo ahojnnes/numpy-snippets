40741c918169cbca3b0ab82f21add0e65860d98c
Johannes Schönberger <jschoenberger@demuc.de>
2012-03-11 23:06:27 +0100

    added affine transformation

--- transformations.py
+++ transformations.py
@@ -2,6 +2,64 @@
 import numpy as np
 
 
+def make_affine(src, dst, explicit=False):
+    '''
+    Determine parameters of 2D affine transformation in the order:
+        a0, a1, a2, b0, b1, b2
+    where the transformation is:
+        X = a0+a1*x+a2*y
+        Y = b0+b1*x+b2*y
+    It is capable of determining the over-, well- and under-determined result
+    with the least-squares method.
+    Source and destination coordinates must be Nx2 matrices (x, y).
+
+    If explicit is True explicit parameters are returned in the order:
+        a0, a1, mx, my, alpha, beta
+    where the transformation is:
+        X = mx*x*cos(alpha)-my*y*sin(alpha+beta)
+        Y = mx*x*sin(alpha)+my*y*cos(alpha+beta)
+    '''
+
+    rows = src.shape[0]
+    A = np.zeros((rows*2, 6))
+    A[:rows,0] = 1
+    A[:rows,1] = src[:,0]
+    A[:rows,2] = src[:,1]
+    A[rows:,3] = 1
+    A[rows:,4] = src[:,0]
+    A[rows:,5] = src[:,1]
+    b = np.zeros((rows*2,))
+    b[:rows] = dst[:,0]
+    b[rows:] = dst[:,1]
+    params = np.linalg.lstsq(A, b)[0]
+    if explicit:
+        a0, a1 = params[:0], params[:3]
+        alpha = math.atan2(params[4], params[1])
+        beta = math.atan2(params[5], -params[2]) - alpha
+        mx = params[1] / math.cos(alpha)
+        mx = params[5] / math.cos(alpha+beta)
+        return a0, a1, mx, my, alpha, beta
+    else:
+        return params
+
+def affine_transform(coords, params, inverse=False):
+    '''
+    Applies projective transformation to a Nx2 coordinate matrix (x, y) with
+    parameters returned by ``make_affine`` with explicit=False.
+    '''
+
+    a0, a1, a2, b0, b1, b2 = params
+    x = coords[:,0]
+    y = coords[:,1]
+    out = np.zeros(coords.shape)
+    if inverse:
+        out[:,0] = (a2*(y-b0)-b2*(x-a0)) / (a2*b1-a1*b2)
+        out[:,1] = (b1*(x-a0)-a1*(y-b0)) / (a2*b1-a1*b2)
+    else:
+        out[:,0] = a0+a1*x+a2*y
+        out[:,1] = b0+b1*x+b2*y
+    return out
+
 def make_projective(src, dst):
     '''
     Determine parameters of 2D projective transformation in the order:
